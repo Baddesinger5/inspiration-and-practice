0104dfcf2f8116afc8273810379701d5bd0f9f6b
Vlad <baddesinger5@gmail.com>
2021-02-12 12:57:32 +0300

    add full sticky menu

--- digital agency/js/main.js
+++ digital agency/js/main.js
@@ -46,3 +46,32 @@ document.addEventListener('DOMContentLoaded', function() {
             opacity -= opacity * 0.1;
         }, 10);  
   }
+
+
+  //sticky menu change bg color
+
+  let ID_MENU = "header";//ИД меню
+  let lightLogo = document.querySelector('#logo-light');
+  let darkLogo = document.querySelector('#logo-dark');
+
+  window.onload = function(){
+      document.getElementById(ID_MENU).classList.add("bgColorTransparent");
+  window.addEventListener("scroll",function(){
+    // let element = document.getElementById(ID_MENU);
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;//текущая позиция скролла
+    if(scrollTop == 0){
+      
+      document.getElementById(ID_MENU).classList.remove("bgColorWhite");
+      document.getElementById(ID_MENU).classList.add("bgColorTransparent");     
+      lightLogo.style.display = 'block';
+      darkLogo.style.display = 'none';
+
+    }
+    else{
+      document.getElementById(ID_MENU).classList.remove("bgColorTransparent");
+      document.getElementById(ID_MENU).classList.add("bgColorWhite");
+      lightLogo.style.display = 'none';
+      darkLogo.style.display = 'block';
+    }
+  });
+  };
