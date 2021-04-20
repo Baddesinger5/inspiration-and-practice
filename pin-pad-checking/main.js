c23439ddb78d2eb773d558b799a493c347ee98ab
Vlad <baddesinger5@gmail.com>
2021-04-20 19:29:09 +0300

    add new class for only numbers

--- pin-pad-checking/main.js
+++ pin-pad-checking/main.js
@@ -3,18 +3,19 @@
 //сдеалть эффект неправильного ввода цифр
 //при неправильном вводе удалить все из инпута, после эффекта
 //при правильном пароле, либо сделать переход на страницу, либо скрыть пинпад и показать какоето сообщение
-let input = document.querySelector('input').value;
+let input = document.querySelector('input');
 let btns = document.querySelectorAll('.btn');
 let times = 4;
+const backspace = document.querySelector('.backspace');
+const done = document.querySelector('.done');
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(event) {
-        if (times > 0) {
-            event.preventDefault();
+        if (times > 0 && event.target.matches('.number')) {
             document.querySelector('input').value += btn.value;
             times--;
-        }
-
-        
+        } 
+        console.log(event.target);
     });
 });
+
