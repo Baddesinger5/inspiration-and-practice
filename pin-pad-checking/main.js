1ccbe63e66b9b698bd61f9b831a117b091bce0c5
Vlad <baddesinger5@gmail.com>
2021-04-21 19:42:58 +0300

    create all functions and add second-page.html for redirect if password is true

--- pin-pad-checking/main.js
+++ pin-pad-checking/main.js
@@ -1,21 +1,36 @@
-//создать условие для харанее заданного кода авторизации (придумать код из 4 цифр)
-//сделать возможность удаления символов из инпута
-//сдеалть эффект неправильного ввода цифр
-//при неправильном вводе удалить все из инпута, после эффекта
-//при правильном пароле, либо сделать переход на страницу, либо скрыть пинпад и показать какоето сообщение
 let input = document.querySelector('input');
 let btns = document.querySelectorAll('.btn');
 let times = 4;
 const backspace = document.querySelector('.backspace');
 const done = document.querySelector('.done');
+let container = document.querySelector('.container');
+let body = document.querySelector('body')
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(event) {
         if (times > 0 && event.target.matches('.number')) {
-            document.querySelector('input').value += btn.value;
+            input.value += btn.value;
             times--;
         } 
-        console.log(event.target);
     });
 });
 
+done.addEventListener('click', function() {
+    if (input.value === '1234') {
+        window.location.href = 'second-page.html';
+    } else {
+        input.classList.add('bad-gateway');
+        if (input.classList.contains('bad-gateway')) {
+            setTime = setTimeout(function() {
+                input.classList.remove('bad-gateway');
+                input.value = '';
+                times = 4;
+            }, 300)
+        }
+    }
+});
+
+backspace.addEventListener('click', function() {
+    input.value = input.value.substring(0, input.value.length - 1)
+    times++;
+})
